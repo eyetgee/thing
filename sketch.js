69def6ed2b92bf2e14220ec61a494653b1f23419
eyetgee <244296@bwls.net>
2016-10-13 15:28:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
 }
 
 function draw() {
-  background( 255 , 167 , 79 );
+  background( 255 , 167 , 79 , 10 );
   triangle( mouseX+40 , mouseY+30 , mouseX-40 , mouseY+30, mouseX , mouseY-40);
   
 }
